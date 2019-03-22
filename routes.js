f87419e5e137cf145769ae31aa296be213bd35c8
Cristian Felipe Buitrago Rodriguez <36422189+buty619@users.noreply.github.com>
2019-03-22 11:45:13 -0500

    p

--- routes.js
+++ routes.js
@@ -9,17 +9,17 @@ const polls = require("./controller/polls");
 const middlewares= require('./middlewares');
 
 router.get("/register", registrations.new);
-router.post("/register/new",registrations.create);
+router.post("/register",registrations.create);
 router.get("/logIn", session.new);
 router.post("/logIn", session.create);
 router.get("/logOut", session.logOut);
 
 router.get("/polls",middlewares.requireUser,polls.init);
 router.get("/polls/new",middlewares.requireUser,polls.newPoll);
-router.post("/polls/new/create",middlewares.requireUser,polls.create);
+router.post("/polls/new/create",middlewares.requireUser,polls.create); //modificar polls/new para listar y generar
 router.get("/polls/:id",polls.poll);
 router.patch("/polls/:id/vote",polls.vote);
-router.delete('/polls/:id/delete',middlewares.requireUser,polls.delete);
+router.delete('/polls/:id/delete',middlewares.requireUser,polls.delete); // hasta /polls/:id .. con el metodo delete
 router.get("/polls/:id/result",polls.result);
 
 
