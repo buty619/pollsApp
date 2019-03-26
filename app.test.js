e902471e3c7d80c39cc38640440c0a8ed7a7d6dc
Cristian Felipe Buitrago Rodriguez <36422189+buty619@users.noreply.github.com>
2019-03-26 13:45:18 -0500

    p

--- app.test.js
+++ app.test.js
@@ -1,6 +1,7 @@
 const request = require("supertest");
-const app = require("./app.js");
 const mongoose = require("mongoose");
+const app = require("./app");
+
 
 beforeEach(async () => {
   // antes de cada prueba limpiamos todas las colecciones para iniciar con una
@@ -16,7 +17,7 @@ afterAll(async () => {
 
 describe("/", ()=>{
   test("GET responds whit success code(200)", async () => {
-    const responce = await request(app.get("/"));
+    const responce = await request(app).get("/");
     expect(responce.statusCode).toBe(200);
   });
 })
--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const PORT = process.env.PORT  || 3000;
 const app = require("./app.js");
-
 
 app.listen(PORT, () => console.log("Inició en puerto .." + PORT));
